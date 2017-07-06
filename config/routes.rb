49a21ca794d9daedf4a286b1490596ef5a0194e6
Katsu <keesmark@gmail.com>
2017-07-06 05:21:18 +0000

    user login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   root to: 'toppages#index'
   
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  
+  get 'singup', to: 'users#new'
+  resources :users, only:[:index, :show, :new, :create]
+  
 end
